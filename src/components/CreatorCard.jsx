1eca10dd18622eb60f751a9ade75fc65416b683a
Ian Duhamel <ianduhamelhayes@gmail.com>
2023-01-26 09:03:50 -0300

    Adding creators section

--- src/components/CreatorCard.jsx
+++ src/components/CreatorCard.jsx
@@ -1,17 +1,41 @@
 import Button from "./Button"
 
-const CreatorCard = ({ profileImg, bannerImg }) => {
+const CreatorCard = ({
+	profileImg,
+	bannerImg,
+	name,
+	art,
+	description,
+	followers,
+	following,
+}) => {
 	return (
 		<section className='creator-card'>
-			<img src={profileImg} alt='xdxd' className={`trending-img`} />
+			<img src={bannerImg} alt='xdxd' className={`banner-img`} />
+			<img src={profileImg} alt='xdxd' className={`creator-img`} />
 			<div className='creator-description'>
-				<div className='description'>
-					<h3 className='creator-name'></h3>
-					<p className='author'>By</p>
+				<div className='description-text'>
+					<h3 className='creator-name'>{name}</h3>
+					<p className='author'>{description}</p>
+				</div>
+				<div className='buttons'>
 					<Button text={"Follow"} color={"transparent bid highlight-btn"} />
 					<Button text={"Message"} color={"transparent bid highlight-btn"} />
 				</div>
-				<div className='creator-data'></div>
+				<div className='creator-data'>
+					<div className='artworks'>
+						<h3 className='data-title'>Art Works</h3>
+						<h4 className='data-number'>{art}</h4>
+					</div>
+					<div className='artworks'>
+						<h3 className='data-title'>Followers</h3>
+						<h4 className='data-number'>{followers}</h4>
+					</div>
+					<div className='artworks'>
+						<h3 className='data-title'>Following</h3>
+						<h4 className='data-number'>{following}</h4>
+					</div>
+				</div>
 			</div>
 		</section>
 	)
--- src/components/PopularCreators.jsx
+++ src/components/PopularCreators.jsx
@@ -1,13 +1,22 @@
 import FirstCreator from "../img/creators/Ellipse 304.png"
 import firstBg from "../img/creators/Subtract.png"
 import CreatorCard from "./CreatorCard"
+import "./styles/creatorCard.css"
 
 const PopularCreators = () => {
 	return (
 		<section className='popular-creators'>
 			<header className='howtobecome-header'>POPULAR CREATORS</header>
 			<div className='cards-wrapper'>
-				<CreatorCard profileImg={FirstCreator} bannerImg={firstBg} />
+				<CreatorCard
+					profileImg={FirstCreator}
+					bannerImg={firstBg}
+					name={"Alexa Brains"}
+					description={"Specialized in creating monkey NFTs"}
+					art={47}
+					followers={321}
+					following={42}
+				/>
 			</div>
 		</section>
 	)
